626f7e6d04a26ae233d0761477bbea595a5f8c8e
aakash015 <67652208+aakash015@users.noreply.github.com>
2020-12-18 09:22:31 +0530

    Create aakash015.cpp

---
+++ DSA Marathon/Day 4/Solution/aakash015.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a,b;
+        cin>>a>>b;
+        
+        sort(a.begin(),a.end());
+        
+        sort(b.begin(),b.end());
+        
+        if(a==b)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+}
